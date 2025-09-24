aa74fe3fa97f6e12ff49f1e0c87b4d59dd4660b2
Wetan <162630150+Iwetan77@users.noreply.github.com>
2025-09-24 12:02:29 -0600

    Update hello.asm

--- hello_asm/hello.asm
+++ hello_asm/hello.asm
@@ -1,9 +1,9 @@
 section .data
-    msg db "Hello, world!", 0xA  ; Our string, 0xA is the newline character
+    msg db "Hello, world!", 0xA  ; string 0xA is the newline character
     len equ $ - msg             ; Length of the string
 
 section .text
-    global _start               ; Entry point for the program
+    global _start               ; Entry point for program
 
 _start:
 
@@ -11,8 +11,8 @@ _start:
     mov rdi, 1                  ; File descriptor for standard output (rdi = 1)
     mov rsi, msg                ; Address of the string to write (rsi = msg)
     mov rdx, len                ; Length of the string (rdx = len)
-    syscall                     ; Execute the system call
+    syscall                     ; Make system call
 
     mov rax, 60                 ; System call for sys_exit (rax = 60)
     mov rdi, 0                  ; Exit code (rdi = 0 for success)
-    syscall                     ; Execute the system call
+    syscall                     ; Make system call
